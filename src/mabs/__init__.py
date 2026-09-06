266b3cdc48e834002bcf3681a9943f514505497a
HellWithCode0 <aryaman6949@gmail.com>
2026-09-06 09:36:21 +0200

    v2.1 public: full SUMMARY, version 2.1.0

--- src/mabs/__init__.py
+++ src/mabs/__init__.py
@@ -4,7 +4,7 @@ from mabs.config import MABSConfig
 from mabs.algorithm import run_mabs, MABSResult
 from mabs.timing import NestedTimers, TimingSample
 from mabs.mixture import MixtureStats, compute_mixture, delta_decomposition
-from mabs.reporting import boundary_ratios, offered_load, SMeas, summarize
+from mabs.reporting import boundary_factor, offered_load, SMeas, summarize
 from mabs.confidence import confidence_score, ConfidenceConfig, calibrate_auto_threshold
 from mabs.adaptive import AdaptiveConfig, AdaptiveState, stream_shot_adaptive_timed
 
@@ -17,7 +17,7 @@ __all__ = [
     "MixtureStats",
     "compute_mixture",
     "delta_decomposition",
-    "boundary_ratios",
+    "boundary_factor",
     "offered_load",
     "SMeas",
     "summarize",
@@ -29,4 +29,4 @@ __all__ = [
     "stream_shot_adaptive_timed",
 ]
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
